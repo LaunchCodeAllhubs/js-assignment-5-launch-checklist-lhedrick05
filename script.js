f97f79d839b836e82878f7226a124ad0d4665c18
Landon <landon.hedrick05@gmail.com>
2023-08-06 22:23:51 -0500

    Fixed last error, webpage working as designed

--- script.js
+++ script.js
@@ -18,15 +18,18 @@ window.addEventListener("load", function() {
             formSubmission(document, list, pilot, coPilot, fuelLevel, cargoMass);
             event.preventDefault();
         };
+
     });
 
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse = myFetch();
    console.log(listedPlanetsResponse);
+
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
+
    }).then(function () {
 
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
